2d1112024658f202dc713011a42e230ee71d5562
jerome83136 <jerome83136@gmail.com>
2024-10-16 15:17:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,7 @@ import network
 import espnow
 from time import sleep
 from machine import UART
-#uart = UART(0, 115200) # 1st argument: UART number: Hardware UART #1
 uart = UART(0, baudrate=115200, bits=8, parity=None, stop=1)
-
-#while True:
-# uart.write('keepalive\n')
-# sleep(1)
 
 def espnow_rx():
 
@@ -19,15 +14,10 @@ def espnow_rx():
     e = espnow.ESPNow()                  # Enable ESP-NOW
     e.active(True)
 
-    #peer = b'p\x04\x1d3T\x88'   # MAC address of peer's wifi interface
-    #e.add_peer(peer)                     # Sender's MAC registration
-
     while True:
         host, msg = e.recv()
-#        uart.write('keepalive\n')
         if msg:                          # wait for message
              uart.write(msg.decode('utf-8')+'\n')
-#            print(msg.decode('utf-8'))
 
 if __name__ == "__main__":
     espnow_rx()
