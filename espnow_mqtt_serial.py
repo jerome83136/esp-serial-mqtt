c2baee2336787eec9fedd76de9ab60e6236cdb79
jerome83136 <jerome83136@gmail.com>
2024-10-16 15:16:05 +0200

    Update espnow_mqtt_serial.py

--- espnow_mqtt_serial.py
+++ espnow_mqtt_serial.py
@@ -15,20 +15,19 @@ ser = serial.Serial('/dev/ttyUSB3', baudrate = 115200, timeout = 5)
 print(ser.name) 
 
 #mqtt
-broker="mqtt.maison.lan"
-port=1883
-client_id= "espnowhub"
-topic= "espnowtest"
-#client1= paho.Client(paho.CallbackAPIVersion.VERSION1, client_id)
+broker="1.2.3.4" #broker IP address or DNS name
+port=1883 #broker port number
+client_id= "espnowhub" #Client ID of this device when connectint to MQTT broker
+topic= "espnowtest" #MQTT topic where all messages will be sent by this device
 client1= paho.Client(client_id)
 
 #program
+#endless loop to read the output of the serial connection with the ESP device
 while True:
- if (ser.inWaiting() > 0):
-  #serial_data = ser.readlines().decode('utf-8').rstrip().strip()
-  serial_data = ser.readlines()
-  for line in serial_data:
-   device = (json.loads(line.decode('utf-8').rstrip().strip())['device'])
+ if (ser.inWaiting() > 0): # detect new data on serial output
+  serial_data = ser.readlines() # read all lines and store them in "serial_data" variable
+  for line in serial_data: # for each line; do bellow actions
+   device = (json.loads(line.decode('utf-8').rstrip().strip())['device']) # extract the device name from serial data
    #debug mode
    if args == "debug":
     print(line.decode('utf-8').rstrip().strip())
